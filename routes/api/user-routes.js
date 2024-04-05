2fcbe64cc32d8b4207326ddf97cc45008759da71
Yogi Spice <caseynnewman@yahoo.com>
2024-04-05 12:42:42 -0400

    working on route to add friend to user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,7 +7,7 @@ const { User, Thought } = require('../../models');
 // GET all users
 router.get('/', async (req, res) => {
     try {
-        const userData = await User.find({}).lean()
+        const userData = await User.find({}).lean();
         res.status(200).json(userData);
     } catch (err) {
         res.status(500).json(err);
@@ -15,13 +15,21 @@ router.get('/', async (req, res) => {
 });
 
 // GET a single user by _id and populated thought and friend data
-
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findById(req.params.id).lean();
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // POST a new user
 router.post('/', async (req, res) => {
     try {
-        const newUser = await User.create(req.body)
-        res.status(200).json(newUser);
+        const newUser = await User.create(req.body);
+        console.log(newUser);
+        res.status(200).json({ message: 'New user added successfully! '});
     } catch (err) {
         res.status(500).json(err);
     }
@@ -32,8 +40,9 @@ router.put('/:id', async (req, res) => {
     try{
         const updatedUser = await User.updateOne({
             _id: new ObjectId(req.params.id)
-        }, req.body)
-        res.status(200).json(updatedUser);
+        }, req.body);
+        console.log(updatedUser);
+        res.status(200).json({ message: 'User updated successfully! '});
     } catch (err) {
         res.status(500).json(err);
     }
@@ -42,10 +51,11 @@ router.put('/:id', async (req, res) => {
 // DELETE user by _id
 router.delete('/:id', async (req, res) => {
     try {
-        const deleteUser = await User.deleteOne({
+        const deletedUser = await User.deleteOne({
             _id: new ObjectId(req.params.id)
-        })
-        res.status(200).json(deleteUser);
+        });
+        console.log(deletedUser);
+        res.status(200).json({ message: 'User deleted successfully! '});
     } catch {
         res.status(500).json(err);
     }
@@ -56,11 +66,28 @@ router.delete('/:id', async (req, res) => {
 
 
 
-
-
 // endpoint /api/users/:userId/friends/:friendId
 
 // POST add new friend to user's friend list
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        console.log(userId)
+        console.log(friendId)
+        // if (!user) {
+        //     return res.status(404).json({ message: 'User not found!' });
+        // }
+        user.friends.push(friendId);
+        await user.save();
+        res.status(200).json({ message: 'New friend added successfully!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
 // DELETE remove friend from user's friend list
 
 
